56eec4f95a1c580e5a6c2b53925be4513474e931
chenjie3 <chenjie3@37.com>
2023-01-30 16:11:51 +0800

    基础语法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,58 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 
 defineProps({
   msg: String,
 })
 
+// 用来改变状态、触发更新的函数
+function increment(){
+  count.value++;
+}
+
+// 响应式状态
 const count = ref(0)
+const lists = ['zs' ,'ls' ,'ww']
+
+// 普通状态
+const rawHtml = '<span style="color: red">This should be red.</span>';
+const dynamicId = 'myApp';
+const isButtonDisabled = true;
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper'
+}
+
+// 生命周期钩子
+onMounted(() => {
+  console.log(`The initial count is ${count.value}`)
+})
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increment">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h2>模板语法</h2>
+  <h3>文本插值</h3>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+
+  <h3>Attribute 绑定</h3>
+  <div v-bind:id="dynamicId">my app</div>
+  <button :disabled="isButtonDisabled">Button</button>
+  <div v-bind="objectOfAttrs">some demo</div>
+
+  <ul>
+    <li v-for="(item,index) in lists" :id="`list-${index}`">{{item}}</li>
+  </ul>
 </template>
 
 <style scoped>
